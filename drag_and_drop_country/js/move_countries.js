27899c364ded23e310be55d335894293a70af84a
shivam jain <shivam@vinsol.com>
2017-09-12 15:05:57 +0530

    improved UI

--- drag_and_drop_country/js/move_countries.js
+++ drag_and_drop_country/js/move_countries.js
@@ -10,36 +10,57 @@ CountriesMover.prototype.initialize = function() {
 CountriesMover.prototype.bindEvents = function() {
   this.bindDragEvent();
   this.bindDropEvent();
+  this.bindClickEvent();
 };
 
 CountriesMover.prototype.bindDragEvent = function() {
+  var _this = this;
   this.$countries.draggable({
     revert: true,
     revertDuration: 0,
-    drag: function(event, ui) {
-      ui.helper.addClass('ui-state-highlight');
-    }
+    drag: function(event, ui) { _this.handleDragEvent(this);}
   });
 };
 
 CountriesMover.prototype.bindDropEvent = function() {
-  var $this = '';
+  var _this = this;
   this.$list.droppable({
-    drop: function(event, ui) {
-      $this = $(this);
-      // country is not dropped into the list which is already containing it
-      if (!$.contains($this[0], ui.helper[0])) {
-        $this.append(ui.helper);
-      }
-      ui.helper.removeClass('ui-state-highlight');
-    }
+    drop: function(event, ui) { _this.handleDropEvent(this, ui.helper);}
   });
+};
+
+CountriesMover.prototype.bindClickEvent = function() {
+  var _this = this;
+  this.$countries.on('click', function() { _this.handleClickEvent(this);});
+};
+
+CountriesMover.prototype.handleDragEvent = function(country) {
+  var $country = $(country);
+  this.$countries.removeClass('ui-state-highlight');
+  $country.addClass('ui-state-highlight');
+};
+
+CountriesMover.prototype.handleDropEvent = function(list, country) {
+  var $list = $(list),
+      $country = $(country);
+
+  // country is not dropped into the list which is already containing it
+  if (!$.contains($list[0], $country[0])) {
+    $list.append(country);
+  }
+  $country.removeClass('ui-state-highlight');
+};
+
+CountriesMover.prototype.handleClickEvent = function(country) {
+  var $country = $(country);
+  this.$countries.removeClass('ui-state-highlight');
+  $country.addClass('ui-state-highlight');
 };
 
 // starts ---------------------------
 $(function() {
   var options = {
-    $lists: $("[data-name^='list']"),
+    $lists: $("[data-name='list']"),
     $countries: $("[data-name='country']")
   },
       countryMover = new CountriesMover(options);
